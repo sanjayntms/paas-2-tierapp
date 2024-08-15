d853126912210a18bf367219d065cf1fcaf117b3
sanjayntms <sanjayntms@yahoo.com>
2024-08-15 16:53:50 +0530

    Update submit_inquiry.php

--- submit_inquiry.php
+++ submit_inquiry.php
@@ -1,20 +1,18 @@
 <?php
 include 'db.php'; // Include database connection
 require_once 'vendor/autoload.php'; // Include Azure Blob Storage SDK
-$sasToken = "?sp=racwdl&st=2024-08-15T08:02:29Z&se=2024-08-16T16:02:29Z&spr=https&sv=2022-11-02&sr=c&sig=u0FWwZeTmdNXZ5eQ%2B4bLIA85YOdbbQIvlphup8EY03E%3D";
 
 use MicrosoftAzure\Storage\Blob\BlobRestProxy;
 use MicrosoftAzure\Storage\Blob\Models\CreateBlockBlobOptions;
 
-// Azure Storage Account credentials
-// $connectionString = "DefaultEndpointsProtocol=https;AccountName=your_account_name;AccountKey=your_account_key";
-$blobClient = BlobRestProxy::createBlobService($sasToken);
-
 // SAS Token for the container
-// $sasToken = getenv('AZURE_STORAGE_SAS_TOKEN');
 $sasToken = "?sp=racwdl&st=2024-08-15T08:02:29Z&se=2024-08-16T16:02:29Z&spr=https&sv=2022-11-02&sr=c&sig=u0FWwZeTmdNXZ5eQ%2B4bLIA85YOdbbQIvlphup8EY03E%3D";
 
 $containerName = "inquiry-images"; // Replace with your container name
+
+// Azure Storage Account credentials (not needed for SAS but may be used for other operations)
+$connectionString = "DefaultEndpointsProtocol=https;AccountName=your_account_name;AccountKey=your_account_key";
+$blobClient = BlobRestProxy::createBlobService($connectionString);
 
 // Check if the form is submitted
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -27,13 +25,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $fileTmpPath = $_FILES['photo']['tmp_name'];
         $fileName = $_FILES['photo']['name'];
 
-        // Upload the file to Azure Blob Storage
+        // Upload the file to Azure Blob Storage with SAS Token
         $content = fopen($fileTmpPath, "r");
         $blobName = $fileName;
         $blobClient->createBlockBlob($containerName, $blobName, $content);
 
         // Construct the URL with SAS token
-        $photoUrl = "https://ntmsphpsa.blob.core.windows.net/$containerName/$blobName$sasToken";
+        $photoUrl = "https://your_account_name.blob.core.windows.net/$containerName/$blobName$sasToken";
     }
 
     // Insert the inquiry into the database
