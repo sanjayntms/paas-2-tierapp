8390b66d3126f81a67b34ae19a46a2578905adff
sanjayntms <sanjayntms@yahoo.com>
2024-08-16 21:49:08 +0530

    added login logout php section

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -67,3 +74,10 @@
     </div>
 </body>
 </html>
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit();
+}
+?>
