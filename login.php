f1572b5309f9147f73901f69d22674c0bdf6f96b
sanjayntms <sanjayntms@yahoo.com>
2024-08-25 15:42:22 +0530

    Update login.php

--- login.php
+++ login.php
@@ -30,7 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Login South East Asia</title>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap');
 
